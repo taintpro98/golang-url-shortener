9f5fa27af51cfe7ffd3777e92b1c3d999ed71e7f
taintpro98 <taideptrai02@gmail.com>
2024-10-27 19:24:41 +0700

    echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo "Running database migrations..."
+echo $POSTGRES_HOST
+echo $POSTGRES_DB
+echo $POSTGRES_USER
 /bin/migration -dir /app/migrations up
 
 echo "Starting the server..."
